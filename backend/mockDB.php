b3bbe48ad60aba17fa3080318e69983c5ab82c78
DanielStrensHelha <LA217024@student.helha.be>
2023-03-21 17:35:08 +0100

    Updated the backend response to send some users

--- backend/mockDB.php
+++ backend/mockDB.php
@@ -4,5 +4,19 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization');
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET')
-    echo json_encode(['message' => 'Service is up']);
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['list']))
+    echo json_encode([
+        '1' => [
+            'nom' => 'Strens',
+            'prenom' => 'Daniel',
+            'matricule' => 'LA217024'
+        ],
+        
+        '2' => [
+            'nom' => 'Matteo',
+            'prenom' => 'Firenze',
+            'matricule' => 'LA217891'
+        ]
+    ]);
+}
