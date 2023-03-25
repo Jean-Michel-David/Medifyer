74b71791656e2d2ee3bb191db52eb3de83fd648c
Jean-Michel-David <la218008@student.helha.be>
2023-03-25 23:35:07 +0100

    Form2 now checks mesh validity

--- frontend/src/app/recherche/form2/form2.component.ts
+++ frontend/src/app/recherche/form2/form2.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-form2',
@@ -9,15 +11,42 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 export class Form2Component implements OnInit{
   @Input()
   secondPart!:FormGroup;
+  @ViewChild('formTerms')
+  formTerms!: ElementRef;
 
 
 
-  constructor(private fb : FormBuilder){}
+  constructor(private fb : FormBuilder,private http:HttpClient){}
 
   ngOnInit(): void {
   }
 
   addTerm(group:string,column:string){
     ((this.secondPart.controls[group] as FormGroup).controls[column] as FormArray).push(this.fb.control(''));
+  }
+
+  checkMeshTerm(event:Event,group:string,term:number){
+    if((((this.secondPart.controls[group] as FormGroup).controls['mesh'] as FormArray).at(term).value).length > 1){
+      let req = this.http.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=mesh&term='
+      + ((this.secondPart.controls[group] as FormGroup).controls['mesh'] as FormArray).at(term).value
+      +'[Mesh]',
+      {responseType: 'text'}).subscribe(
+        response => {
+          let parser = new DOMParser();
+          let xml = parser.parseFromString(response, 'text/xml');
+          let count:number = parseInt(xml.getElementsByTagName('eSearchResult')[0].getElementsByTagName('Count')[0].innerHTML);
+          if(count>0){
+            (event.target as HTMLElement).classList.add('valid');
+            (event.target as HTMLElement).classList.remove('notvalid');
+          } else {
+            (event.target as HTMLElement).classList.add('notvalid');
+            (event.target as HTMLElement).classList.remove('valid');
+          }
+          req.unsubscribe();
+      });
+    } else {
+      (event.target as HTMLElement).classList.remove('valid');
+      (event.target as HTMLElement).classList.remove('notvalid');
+    }
   }
 }
