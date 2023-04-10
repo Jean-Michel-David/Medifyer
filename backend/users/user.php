c43a126d8f67cfbf475b7976d667496320e72a2d
Loris-Clement <la216834@student.helha.be>
2023-04-10 21:14:41 +0200

    adding jwt to user.php

--- backend/users/user.php
+++ backend/users/user.php
@@ -3,11 +3,13 @@
 require('User.class.php');
 require('../database/dbConnection.php');
 require('UserManager.php');
+require ('../credentials.php');
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: content-type");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 
+$credentials = new Credentials();
 $userManager = new UserManager($db);
 $regex = '/la[0-9]{6}@student\.helha\.be/';
 $pwdhashed = '';
@@ -19,32 +21,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $json_obj = json_decode(file_get_contents('php://input'), true);
   $user = new User();
   $user->setId($json_obj['id']);
-  if ($json_obj['firstname'] != null && strlen($json_obj['firstname'])<50) {
-    $user->setFirstname($json_obj['firstname']);
-  }else{
+  if ($json_obj['firstname'] == null && strlen($json_obj['firstname'])>50) {
     http_response_code(400);
     "Prénom Invalide ! ";
+    exit;
   }
-  if ($json_obj['lastname'] != null & strlen($json_obj['lastname'])<50) {
-    $user->setLastname($json_obj['lastname']);
-  } else {
+  if ($json_obj['lastname'] == null & strlen($json_obj['lastname'])>50) {
     http_response_code(400);
     echo "Nom Invalide ! ";
+    exit;
   }
-  if ($json_obj['email'] != null && strlen($json_obj['email'])<50 && preg_match($regex, $json_obj['email'])) {
-    $user->setEmail($json_obj['email']);
-  } else {
+  if ($json_obj['email'] == null && strlen($json_obj['email'])>50 && !preg_match($regex, $json_obj['email'])) {
     http_response_code(400);
     echo "Email Invalide !";
+    exit;
   }
-  if($json_obj['pwd'] != null){
-  $pwdhashed = password_hash($json_obj['pwd'], PASSWORD_BCRYPT, $options);
-    $user->setPwd($pwdhashed);
-  } else {
+  if($json_obj['pwd'] == null){
     http_response_code(400);
     echo "Mot de passe Invalide";
+    exit;
   }
+  $pwdhashed = password_hash($json_obj['pwd'], PASSWORD_BCRYPT, $options);
+  $user->setFirstname($json_obj['firstname']);
+  $user->setLastname($json_obj['lastname']);
+  $user->setEmail($json_obj['email']);
+  $user->setPwd($pwdhashed);
   $user->setPhoto($json_obj['photo']);
   $userManager->saveUser($user);
+  $credentials->createToken($user);
   echo json_encode($user);
 }
