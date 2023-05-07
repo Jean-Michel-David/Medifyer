0bb4cd687cf7514620aa452e36dabb78329f8bd7
Loris-Clement <la216834@student.helha.be>
2023-05-07 14:53:44 +0200

    Deleting useless function in UserManager

--- backend/users/UserManager.php
+++ backend/users/UserManager.php
@@ -9,24 +9,9 @@ class UserManager
     $this->db = $db;
   }
 
-  // Function that searches for the user in the database using their email address. 
-  public function getUser($email)
-  {
-    $sql="SELECT user_id, nom_user, prenom_user, psw_user, pfp_user, admin_user, email_user from users WHERE email_user=:email";
-    $result = array();
-    try {
-      $select = $this->db->prepare($sql);
-      $select->execute(array('email'=>$email));
-      $result = $select->fetch(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-      die($e);
-    } finally {
-      $select->closeCursor();
-    }
-    return $result;
-  }
-
-  // Function that insert an user in the database using data encoded in the registration form.
+  /**
+    @Description Function that insert an user in the database using data encoded in the registration form.
+  */
   public function saveUser(User $user)
   {
     $sql="INSERT INTO users(nom_user, prenom_user, psw_user, pfp_user, admin_user, email_user) VALUES (:nom_user, :prenom_user, :psw_user, :pfp_user, :admin_user, :email_user)";
