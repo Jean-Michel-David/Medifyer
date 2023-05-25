85b75dfbb2d1842574e4c02da068d001357bb101
Daniel Strens <LA217024@student.helha.be>
2023-05-25 17:29:33 +0200

    test.php to verify that phpunit works correctly

--- backend/test/test.php
+++ backend/test/test.php
@@ -1,1 +1,12 @@
-This file is not useless
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+    public function testAddition()
+    {
+        $result = 2 + 2;
+        $this->assertEquals(4, $result);
+    }
+}
