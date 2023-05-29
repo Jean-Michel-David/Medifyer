65958bb5fff3e91746e071141ed3518fd77bac5e
Loris-Clement <la216834@student.helha.be>
2023-05-29 14:05:40 +0200

    adding function for password recuperation code

--- frontend/src/app/page/recup-password/password-code/password-code.component.ts
+++ frontend/src/app/page/recup-password/password-code/password-code.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EmailSenderService } from 'src/app/services/email-sender.service';
 
 @Component({
   selector: 'app-password-code',
   templateUrl: './password-code.component.html',
   styleUrls: ['./password-code.component.css']
 })
-export class PasswordCodeComponent {
+export class PasswordCodeComponent implements OnInit {
+ 
+  submitted = false;
+  verifCode = "";
 
+  verifPwdForm: FormGroup = new FormGroup({
+    verifCode: new FormControl()
+  })
+
+  constructor(
+    private api: EmailSenderService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.verifPwdForm = this.fb.group({
+      verifCode: ['', [Validators.required]]
+    })
+  }
+
+  verifyPwdCode() {
+    this.submitted = false;
+    this.verifCode = this.verifPwdForm.controls['verifCode'].value;
+    const sub = this.api.checkCode(this.verifCode).subscribe(
+      (response) => {
+        if (response.success) {
+          alert(response.message);
+          localStorage.setItem('pwd-code' ,this.verifCode);
+          this.router.navigate(['new-password']);
+        } else {
+          alert(response.message);
+        }
+        sub.unsubscribe();
+      }
+    );
+  }
+
+  onSubmit(){
+    this.submitted = true;
+  }
 }
